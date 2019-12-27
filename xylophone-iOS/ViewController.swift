9ad6994c2ea445e8759eb189be8783e09e5eed5c
Muthu <s.muthukhumar@gmail.com>
2019-12-27 15:49:53 +0800

    1. Icons added 2. Buttons wise sound played 3. Button ripple animation added

--- xylophone-iOS/ViewController.swift
+++ xylophone-iOS/ViewController.swift
@@ -7,14 +7,29 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    var player: AVAudioPlayer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func keyPressed(_ sender: UIButton) {
+    
+        playSound(title: sender.currentTitle!)
+        sender.alpha = 0.5
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+            sender.alpha = 1
+        }
 
-
+    }
+    func playSound(title :String) {
+        let url = Bundle.main.url(forResource: title, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player?.play()
+    }
 }
 
